36047aeca851f856c10ac953b718cbe493f459a1
Jean Chirinos <jeanchirinos17@gmail.com>
2022-04-20 15:57:48 -0500

    ssr for scomponents

--- components/Home/Molecules/PlantillasStack.jsx
+++ components/Home/Molecules/PlantillasStack.jsx
@@ -26,8 +26,8 @@ const S_PlantillasStack = styled.div(
     gap: 0.8rem;
     flex-wrap: wrap;
     position: absolute;
-    bottom: 0;
-    transform: ${!active && 'translateY(100%)'};
+    bottom: -5rem;
+    transform: ${active && 'translateY(-100%)'};
     transition: transform 0.5s;
     box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
   `
